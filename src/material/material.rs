f0451c424a2e2eb8b1a772fdb819a8be232e689f
mengxianglu <2534905725@qq.com>
2023-04-14 20:24:13 +0800

    material update

--- src/material/material.rs
+++ src/material/material.rs
@@ -1,4 +1,103 @@
 use super::*;
 use rand::Rng;
+use std::{intrinsics::powf64, marker::Sync};
 
-//
+#[derive(Debug, Copy, Clone)]
+pub struct Lambertian<T: Texture> {
+    albedo: T,
+}
+
+impl<T: Texture> Lambertian<T> {
+    pub fn new(albedo: T) -> Lambertian<T> {
+        Lambertian { albedo }
+    }
+}
+
+impl<T: Texture + Sync> Material for Lambertian<T> {
+    fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<(Color, Ray)> {
+        let mut scatter_direction = rec.normal + Vector3::random_in_unit_sphere().normalize();
+        if scatter_direction.near_zero() {
+            scatter_direction = rec.normal;
+        }
+
+        let scatted = Ray::new(rec.p, scatter_direction, r_in.time());
+
+        Some((self.albedo.get_color(rec.u, rec.v, &rec.p), scatted))
+
+        // sca
+    }
+
+    fn emitted(&self, _rec: &HitRecord) -> Color {
+        Color::new(0.0, 0.0, 0.0)
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Metallic {
+    albedo: Color,
+    fuzz: f64,
+}
+
+impl Metallic {
+    pub fn new(albedo: Color, fuzz: f64) -> Metallic {
+        Metallic { albedo, fuzz }
+    }
+}
+
+impl Material for Metallic {
+    fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<(Color, Ray)> {
+        let reflected = r_in.dir().reflect(rec.normal).normalize();
+        let scattered = Ray::new(
+            rec.p,
+            reflected + Vector3::random_in_unit_sphere() * self.fuzz,
+            r_in.time(),
+        );
+        if scattered.dir().dot(rec.normal) > 0.0 {
+            Some((self.albedo, scattered))
+        } else {
+            None
+        }
+    }
+}
+
+pub struct Dielectric {
+    pub ir: f64,
+}
+
+impl Dielectric {
+    pub fn new(ir: f64) -> Dielectric {
+        Dielectric { ir }
+    }
+}
+
+fn reflectance(cosine: f64, ref_index: f64) -> f64 {
+    let mut r0 = (1.0 - ref_index) / (1.0 + ref_index);
+    r0 *= r0;
+    r0 + (1.0 - r0) * (1.0 - cosine).powi(5)
+}
+
+impl Material for Dielectric {
+    fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<(Color, Ray)> {
+        let mut rng = rand::thread_rng();
+        let attenuation = Color::new(1.0, 1.0, 1.0);
+        let refraction_ratio = if rec.front_face {
+            1.0 / self.ir
+        } else {
+            self.ir
+        };
+
+        let unit_direction = r_in.dir().normalize();
+        let cos_theta = (-unit_direction).dot(rec.normal).min(1.0);
+        let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
+
+        let cannot_refract = refraction_ratio * sin_theta > 1.0;
+        let direction =
+            if cannot_refract || reflectance(cos_theta, refraction_ratio) > rng.gen::<f64>() {
+                unit_direction.reflect(rec.normal);
+            } else {
+                unit_direction.refract(rec.normal, refraction_ratio);
+            };
+
+        let scattered = Ray::new(rec.p, direction, r_in.time());
+    }
+}
--- src/material/mod.rs
+++ src/material/mod.rs
@@ -2,10 +2,13 @@ use self::material::*;
 
 pub mod material;
 
-use crate::{utility::*, geometry::HitRecord};
+use crate::{geometry::HitRecord, texture::Texture, utility::*};
 
 pub trait Material: Sync {
-    fn emitted(&self, u: f64, v: f64, p: &Point) -> Color;
-    fn scatter(&self, r_in: &Ray, rec: &HitRecord, attenuation: Color, scattered: Color) -> bool;
-
+    fn emitted(&self, _rec: &HitRecord) -> Color {
+        Color::new(0.0, 0.0, 0.0)
+    }
+    fn scatter(&self, _r_in: &Ray, _rec: &HitRecord) -> Option<(Color, Ray)> {
+        None
+    }
 }
